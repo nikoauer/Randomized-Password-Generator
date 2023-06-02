09a598d8fcefceb80f575f7755bb746d77e46b6b
Niko Auer <nikoauer26@gmail.com>
2023-06-02 22:48:23 +1000

    script.js file complete

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,62 @@
-//This var gets the button
-var button = document.querySelector('button');
-var userInput
 
-//This listens for a the click upon pressing the button and gets user input
-button.addEventListener('click', function() {
-    var userInput = window.prompt("Please enter a number between 8 and 128");
-    if (isNaN(userInput) || userInput < 8 || userInput > 128) {
-    userInput = window.prompt("Invalid length or number! Please try again.")
-    } else {
-        
-    }
-  });
+// The variables pull value from id element tags in the index.html and retains value in each respective variable
+var characterAmountInput = document.getElementById('characterAmount');
+var useUppercaseInput = document.getElementById('useUppercase');
+var useLowercaseInput = document.getElementById('useLowercase');
+var useSpecialCharactersInput = document.getElementById('useSpecialCharacters');
+var useNumbersInput = document.getElementById('useNumbers');
+var form = document.getElementById('passwordGeneratorCriteria');
+var passwordDisplay = document.getElementById('passwordDisplay');
 
+// This function prevents the default refreshing of the page happening each time a password is generated
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  var characterAmount = characterAmountInput.value;
+  var useUppercase = useUppercaseInput.checked;
+  var useLowercase = useLowercaseInput.checked;
+  var useSpecialCharacters = useSpecialCharactersInput.checked;
+  var useNumbers = useNumbersInput.checked;
+  var password = generatePassword(characterAmount, useUppercase, useLowercase, useSpecialCharacters, useNumbers);
+  passwordDisplay.innerText = password;
+});
 
+//this function implements the actual password randomization 
+function generatePassword(characterAmount,useUppercase, useLowercase, useSpecialCharacters, useNumbers) {
+  var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var specialCharacters = '!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
+  var numberCharacters = '0123456789';
 
+  // this varible stores and adds the above characters respective to if the checkboxes are selected for the differing characters
+  var allowedCharacters = '';
 
+  // these if statements are triggered true if the checkboxes are selected thus adding the respective character set to the allowedcharacters variable
+  if (useLowercase) {
+    allowedCharacters += lowercaseCharacters;
+  }
+  if (useUppercase) {
+    allowedCharacters += uppercaseCharacters;
+  }
+  if (useNumbers) {
+    allowedCharacters += numberCharacters;
+  }
+  if (useSpecialCharacters) {
+    allowedCharacters += specialCharacters;
+  }
+  // this alert notifies the person that a minimum of one checkbox must be selected in order to generate a password
+  // if the variable is equal to being empty then no checkbox has been selected 
+  if (allowedCharacters === '') {
+    alert('A minimum of one character type is required. Please select one or more for a strong Password to be generated.');
+    return '';
+  }
 
+  //This for loop repeats respective to the number put in the character amount
+  var password = '';
+  for (var i = 0; i < characterAmount; i++) {
+    //inside the loop the random variable stores the result of when Math.random method multiplies a random number to the allowCharacters.length 
+    var random = Math.floor(Math.random() * allowedCharacters.length);
+    // the password stores the randomely generated character within the allowCharacters variable and adds on with each loop
+    password += allowedCharacters[random];
+  }
+  return password;
+}
